9a8e7e92717bc5d655044106fc911148b646b4e3
Calheias <gabrielcalheiasalves1999@gmail.com>
2026-07-16 12:15:12 -0300

    credenciais.h

---
+++ firmware/esp32/access_controller/credenciais.h
@@ -0,0 +1,9 @@
+#ifndef CREDENCIAIS_H
+#define CREDENCIAIS_H
+
+
+constexpr const char* WIFI_SSID = "SAT VILA VERDE 2.4ghz";
+constexpr const char* WIFI_PASSWORD = "BL04504V";
+constexpr const char* API_URL = "http://192.168.1.2:8000/authenticate";
+
+#endif
